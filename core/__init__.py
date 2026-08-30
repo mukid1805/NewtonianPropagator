613772fe2830846a539278846d8308e258cc133e
Your Name <you@example.com>
2026-08-30 09:09:15 +0100

    build: configure pyproject.toml and setuptools-scm dynamic versioning

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,11 @@
 """
 Core astrodynamics engine modules: constants, forces, integrators, propagators, and launchers.
 """
+try:
+    from core._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
 from core.constants import *
 from core.forces import *
 from core.integrators import rk4_step
@@ -8,6 +13,7 @@ from core.propagator import SpacecraftPropagator
 from core.launchers import LaunchVehicle, get_launcher, list_available_launchers
 
 __all__ = [
+    "__version__",
     "SpacecraftPropagator",
     "rk4_step",
     "LaunchVehicle",
